43c65b83487fa01064a1c4b9503b85cc288a7ac5
ronak-kumar-sing <ronakkumarsingh23@lpu.in>
2024-03-30 20:45:34 +0530

    add it

---
+++ app.js
@@ -0,0 +1,8 @@
+    const heading = React.createElement(
+        "h1",
+        {id:"perent "},
+        "How it going ?"
+    );
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    console.log(heading);
+    root.render(heading);
