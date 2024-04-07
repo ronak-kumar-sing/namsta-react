a94653334f2e00b96998259127ce33d60ec0c06f
ronak-kumar-sing <ronakkumarsingh23@lpu.in>
2024-04-07 21:55:05 +0530

    third

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-    const heading = React.createElement(
-        "h1",
-        {id:"perent "},
-        "How it going ?"
-    );
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    console.log(heading);
-    root.render(heading);
+
+// React creatElement ==> react.createElement is Object ==> HTMLelement(reader)
+// const heading = React.createElement("h1",{id: "heading"},"ronak is good boy");
+const jsxheading = <h1 id="heading" >the match will start from  8 pm</h1>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxheading ); 
